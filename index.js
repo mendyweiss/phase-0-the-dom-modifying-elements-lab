d3b12e6b14e1ed1d20f678c01f96abd8efd39c32
Mendy <mendyw94@gmail.com>
2021-12-09 19:55:31 -0500

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+document.getElementById("main").remove();
+
+const newHeader = document.createElement('h1');
+newHeader.id = ('victory');
+
+// document.body.append(newHeader);
+newHeader.textContent ="Your Momma is the champion";
+document.body.append(newHeader);
